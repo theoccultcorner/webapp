03452903cbbe93a5b14c2ac3b031db16f50eeca8
theoccultcorner <theoccultcorner@gmail.com>
2018-04-05 17:08:53 -0700

    created show action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,19 +1,57 @@
 class PortfoliosController < ApplicationController
   def index
-  @portfolio_items = Portfolio.all
-end
-  def new
-  @portfolio_items = Portfolio.new
+    @portfolio_items = Portfolio.all
   end
+
+def angular
+@angular_portfolio_items = Portfolio.angular
 end
+
+
+  def new
+   @portfolio_item = Portfolio.new
+ end
+
 def create
-  @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
   respond_to do |format|
-    if @portfolio_items.save
-      format.html { redirect_to portfolios_path, notice: 'You portfolio item is now live.' }
+    if @portfolio_item.save
+      format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live." }
     else
       format.html { render :new }
     end
   end
+end
+
+def edit
+  @portfolio_item = Portfolio.find(params[:id])
+end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'the record was successfully updated.' }
+      else
+        format.html { render :edit }
+    end
+   end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+ end
+
+ def destroy
+@portfolio_item = Portfolio.find(params[:id])
+
+   @portfolio_item.destroy
+
+   respond_to do |format|
+     format.html { redirect_to portfolios_url, notice: 'item was successfully destroyed.' }
+
+   end
+ end
 end
