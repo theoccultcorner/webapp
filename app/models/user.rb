f546d1a3f900006b481d4c355ba1e328c3f10ecd
theoccultcorner <theoccultcorner@gmail.com>
2018-04-07 09:54:19 -0700

    first name last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+validates_presence_of :name
+
+def first_name
+   if name.try(:split, ",")
+     name.split.first
+   else
+     name
+   end
+ end
+
+ def last_name
+   if name.try(:split, ",")
+     name.split.last
+   end
 end
+end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  resources :topics, only: [:index, :show]
 
-
-  resources :portfolios, except: [:show] do
-    put :sort, on: :collection
-  end
+  resources :portfolios, except: [:show] 
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
 
