b57463f93d3852c6f2750e5fab499cadea3cae4e
theoccultcorner <theoccultcorner@gmail.com>
2018-04-15 21:16:34 -0700

    views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,12 @@ module ApplicationHelper
      (link_to "Register", new_user_registration_path) +
     "<br>".html_safe +
       (link_to "Login", new_user_session_path)
-
+end
      end
-
+def source_helper(layout_name)
+  if session[:source]
+ greeting = "thank you for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+ content_tag(:p, greeting, class: "source-greeting")
+end
   end
 end
